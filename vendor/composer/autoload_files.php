e77f3762d44a6fd052149b595e7345f543a6217d
ls29322 <erikbc_95@hotmail.com>
2017-05-13 16:47:23 +0200

    cabezazos con el resize de las imagenes

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -10,4 +10,6 @@ return array(
     '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
     '023d27dca8066ef29e6739335ea73bad' => $vendorDir . '/symfony/polyfill-php70/bootstrap.php',
     '6a47392539ca2329373e0d33e1dba053' => $vendorDir . '/symfony/polyfill-intl-icu/bootstrap.php',
+    '92c8763cd6170fce6fcfe7e26b4e8c10' => $vendorDir . '/symfony/phpunit-bridge/bootstrap.php',
+    'bd9634f2d41831496de0d3dfe4c94881' => $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
 );
